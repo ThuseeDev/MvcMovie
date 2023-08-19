b6f56050ad5d0fd49d766ced4865810c0bee00ff
Thusitha Sirimanna <thusitha.s@azendtech.com>
2023-08-20 01:11:40 +0530

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int id=1)
+        {
+            ViewData["Name"] = name;
+            ViewData["NumTimes"] = id;
+            return View();
+        }
+    }
+}
